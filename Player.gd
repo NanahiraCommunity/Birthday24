c9ecfad24eb012d7ccffd9528616485391238cd1
Nicocchi <nicocchilover@gmail.com>
2024-02-19 13:21:26 -0500

    base gridmap placement system started

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@export var static_world_map : GridMap = null
+@export var interactables_map : GridMap = null
 
 const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
@@ -29,3 +31,22 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	if (Input.is_action_just_pressed("interact") && is_on_floor()):
+		_dig()
+
+func _dig():
+	if (interactables_map != null && static_world_map != null):
+		var player_global_pos = self.position
+		var cell_pos = to_global(interactables_map.map_to_local(player_global_pos))
+		var cell_item = interactables_map.get_cell_item(cell_pos)
+		var meshes = interactables_map.get_meshes()
+		var mesh_library = interactables_map.mesh_library
+		var item = mesh_library.find_item_by_name("ground_pathOpen2")
+		player_global_pos.y = cell_pos.y / 2
+		static_world_map.set_cell_item(interactables_map.local_to_map(player_global_pos), -1)
+		interactables_map.set_cell_item(interactables_map.local_to_map(player_global_pos), item)
+
+func _check_interactable():
+	pass
+		
