3894035558ea58f88a0902c18c32d3b9340833f6
Nicocchi <nicocchilover@gmail.com>
2024-02-22 13:46:10 -0500

    raycasting for crop collision

--- Player.gd
+++ Player.gd
@@ -11,8 +11,19 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var last_direction = Vector3(1, 0, 0)
 
+@onready var raycast_down : RayCast3D = $RCDown
+@onready var raycast_up : RayCast3D = $RCUp
+@onready var raycast_right : RayCast3D = $RCRight
+@onready var raycast_left : RayCast3D = $RCLeft
+
+var can_place : bool = true
 
 func _physics_process(delta):
+	if (raycast_down.is_colliding() || raycast_up.is_colliding() || raycast_right.is_colliding() || raycast_left.is_colliding()):
+		can_place = false
+	else:
+		can_place = true
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -35,10 +46,10 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	if (Input.is_action_just_pressed("interact") && is_on_floor()):
-		_dig()
+	if (Input.is_action_just_pressed("interact") && is_on_floor() && can_place):
+		_place_crop()
 
-func _dig():
+func _place_crop():
 	if (interactables_map != null && static_world_map != null):
 		var player_global_pos = self.position
 		var cell_pos = to_global(interactables_map.map_to_local(player_global_pos))
@@ -48,14 +59,15 @@ func _dig():
 		var mesh_library = interactables_map.mesh_library
 		var item = mesh_library.find_item_by_name("crops_cornStageA2")
 		player_global_pos.y = cell_pos.y / 2
-		#if (last_direction.x == 1):
-			#player_global_pos.x += 1
-		#elif (last_direction.x == -1):
-			#player_global_pos.x -= 1
-		#elif (last_direction.z == 1):
-			#player_global_pos.z += 1
-		#elif (last_direction.z == -1):
-			#player_global_pos.z -= 1
+		if (last_direction.x == 1):
+			player_global_pos.x += 0.5
+			
+		elif (last_direction.x == -1):
+			player_global_pos.x -= 0.5
+		elif (last_direction.z == 1):
+			player_global_pos.z += 0.5
+		elif (last_direction.z == -1):
+			player_global_pos.z -= 0.5
 		#static_world_map.set_cell_item(interactables_map.local_to_map(player_global_pos), -1)
 		#interactables_map.set_cell_item(interactables_map.local_to_map(player_global_pos), item)
 		var instance = crop_scene.instantiate()
@@ -64,6 +76,9 @@ func _dig():
 		#instance.position.y = player_global_pos.y / 2
 		#instance.position.z = player_global_pos.z / 2
 		interactables_map.add_child(instance)
+		print("placed")
+		
+		
 
 func _check_interactable():
 	pass
